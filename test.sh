5a046ea172a045739c0fda5c2416478a7a06f416
YLU <ylugithub@protonmail.com>
2021-02-02 11:37:20 -0500

    upgrade:anaconda2020.11,monai 0.4.0

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
-IMAGE_NAME=./monai_gpu_0.3.0.simg
+
+#sudo singularity build monai_gpu_0.4.0.simg MONAI_gpu_singulairty/singularity.0.4.0
+
+IMAGE_NAME=./monai_gpu_0.4.0.simg
 singularity exec --nv ${IMAGE_NAME} python -c 'import monai; monai.config.print_config()'
