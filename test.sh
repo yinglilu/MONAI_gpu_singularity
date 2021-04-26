eaa7cf21bbfd32ba0cc8fc3a0d7606ea056cb9f8
ylugithub <ylugithub@protonmail.com>
2021-04-26 09:14:31 -0400

    Update test.sh for 0.5.1

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 
-#sudo singularity build monai_gpu_0.4.0.simg MONAI_gpu_singulairty/singularity.0.4.0
+#sudo singularity build monai_gpu_0.5.1.simg MONAI_gpu_singulairty/singularity.0.5.1
 
-IMAGE_NAME=./monai_gpu_0.4.0.simg
+IMAGE_NAME=./monai_gpu_0.5.1.simg
 singularity exec --nv ${IMAGE_NAME} python -c 'import monai; monai.config.print_config()'
