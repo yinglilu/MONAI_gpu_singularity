ef95f296405c9416ffd95ebbcd2ac1f8af24ba55
ylugithub <ylugithub@protonmail.com>
2021-04-26 09:17:27 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 
-#sudo singularity build monai_gpu_0.5.1.simg MONAI_gpu_singulairty/singularity.0.5.1
+#sudo singularity build monai_gpu_0.5.1.simg MONAI_gpu_singularity/singularity.0.5.1
 
 IMAGE_NAME=./monai_gpu_0.5.1.simg
 singularity exec --nv ${IMAGE_NAME} python -c 'import monai; monai.config.print_config()'
